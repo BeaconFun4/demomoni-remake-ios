911e620c886875f2d19a4c9f42c6200569caa7d8
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-05-02 22:14:50 +0900

    [feat] make RiddleView

---
+++ demomoni-remake-ios/Riddle/RiddleView.swift
@@ -0,0 +1,18 @@
+//
+//  RiddleView.swift
+//  demomoni-remake-ios
+//
+//  Created by 村石 拓海 on 2024/05/02.
+//
+
+import SwiftUI
+
+struct RiddleView: View {
+    var body: some View {
+        Text("なぞなぞ画面")
+            .tabItem {
+                Image(systemName: "questionmark.bubble.fill")
+                Text("なぞなぞ")
+            }
+    }
+}
